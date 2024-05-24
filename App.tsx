13ae179152e80d764e9268796ad146fc6ccdab27
Andrew Dao <daoanhphuc84@gmail.com>
2024-05-24 14:24:55 +0700

    style: font and theme config - add DMSans font and config theme for react native paper

--- App.tsx
+++ App.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {AppNavigator} from './src/navigation';
+import {MD3LightTheme, PaperProvider} from 'react-native-paper';
+
+//config theme
+const theme = {
+  MD3LightTheme,
+  roundness: 8,
+  colors: {
+    ...MD3LightTheme.colors,
+    primary: '#0E0C22',
+    secondary: '#0E0E0E',
+    onPrimary: '#FFFFFF',
+    onSecondary: '#FFFFFF',
+    background: '#FFFFFF',
+    tertiary: '#EBEEEF',
+    onTeritary: '#1A1C16',
+  },
+};
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <AppNavigator />
-    </NavigationContainer>
+    <PaperProvider theme={theme}>
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -6,7 +6,12 @@ import styles from './styles';
 export default function HomeScreen({navigation}: HomeScreenProps) {
   return (
     <View style={styles.container}>
-      <Text>Home screen</Text>
+      <Text style={{fontFamily: 'DMSans-SemiBold'}}>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi, atque.
+        Alias nesciunt sunt perferendis explicabo, officia ab impedit ut magni
+        dolorem, ipsum architecto blanditiis veritatis rem quia? Facere, dicta
+        similique!
+      </Text>
       <Button
         title="Go to Details"
         onPress={() => navigation?.navigate('Details')}
