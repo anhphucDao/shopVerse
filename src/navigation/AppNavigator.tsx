bf9580d8e2947860384578d438478219e52f8219
Andrew Dao <daoanhphuc84@gmail.com>
2024-05-24 10:06:18 +0700

    feat: splash screen - add app navigator and navigation container

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,3 +1,28 @@
-// import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import React, {useEffect, useState} from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import SplashScreen from '../screens/SplashScreen/index';
+import TabNavigator from './TabNavigator';
 
-// const RootStack = createNativeStackNavigator();
+const RootStack = createNativeStackNavigator();
+
+const AppNavigator = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+
+  return (
+    <RootStack.Navigator>
+      {isLoading ? (
+        <RootStack.Screen name="Splash" component={SplashScreen} />
+      ) : (
+        <RootStack.Screen name="Tab" component={TabNavigator} />
+      )}
+    </RootStack.Navigator>
+  );
+};
+
+export default AppNavigator;
