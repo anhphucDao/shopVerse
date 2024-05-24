cc6f00b0a0862ca04e1210ff89483d66018581fa
Andrew Dao <daoanhphuc84@gmail.com>
2024-05-24 17:24:19 +0700

    feat: bottomBar - added bottomBar UI

--- src/navigation/HomeNavigator.tsx
+++ src/navigation/HomeNavigator.tsx
@@ -2,17 +2,15 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from '../screens/HomeScreen/index';
 import DetailsScreen from '../screens/DetailsScreen/index';
-import {useTheme} from 'react-native-paper';
 
 const HomeStack = createNativeStackNavigator();
 
 const HomeNavigator = () => {
-  const theme = useTheme();
-
-  console.log('theme', theme);
-
   return (
-    <HomeStack.Navigator>
+    <HomeStack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}>
       <HomeStack.Screen name="home" component={HomeScreen} />
       <HomeStack.Screen name="Details" component={DetailsScreen} />
     </HomeStack.Navigator>
