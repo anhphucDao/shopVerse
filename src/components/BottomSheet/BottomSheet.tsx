b3a5c6a0d625b00e20565758cd9d875d552350ba
Andrew Dao <daoanhphuc84@gmail.com>
2024-05-26 12:03:23 +0700

    feat: price filter - added price filter UI

--- src/components/BottomSheet/BottomSheet.tsx
+++ src/components/BottomSheet/BottomSheet.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import BottomSheet from '@gorhom/bottom-sheet';
 import {useMemo} from 'react';
 import {View, Text, Button} from 'react-native';
+import PriceFilter from '../PriceFilter';
 import styles from './styles';
 
 export default function BottomSheetComponent({bottomSheetRef}) {
   //use this useEffect to initially hide the bottom sheet
 
-  const snapPoints = useMemo(() => ['50%', '60%'], []);
+  const snapPoints = useMemo(() => ['50%', '75%'], []);
 
   return (
     // <View style={styles.container}>
@@ -17,8 +18,7 @@ export default function BottomSheetComponent({bottomSheetRef}) {
       ref={bottomSheetRef}
       enablePanDownToClose={true}>
       <View style={styles.contentContainer}>
-        <Text style={styles.containerHeadline}>Awesome Bottom Sheet 🎉</Text>
-        {/* <Button title="Close Sheet" onPress={handleClosePress} /> */}
+        <PriceFilter />
       </View>
     </BottomSheet>
     // </View>
